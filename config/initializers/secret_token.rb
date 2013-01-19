2c47d33923aa9690c89384a5aa75b014dfbb131f
Milene <milene.darnis@gmail.com>
2013-01-18 22:26:04 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techypreppy::Application.config.secret_token = '4b15de177f5bf795273ea7f610a568b6af4af0b019dc8ac568e0f2ea9671041ae8fe50dc0282dff01dcb6b6f280e5d0899c3c721dab05ed108216f827bfa81de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Techypreppy::Application.config.session_store :cookie_store, key: '_techypreppy_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Techypreppy::Application.config.session_store :active_record_store
